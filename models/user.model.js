a1338cdae65e706071f1c30556865b2863b0931c
Bhavesh0078 <bhavesh@angularminds.in>
2025-01-07 16:23:24 +0530

    minor backend changes

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     password: { type: String, required: true },
     phoneNumber: { type: String, required: true },
     role: { type: String, enum: ["admin", "user"], default: "user" },
